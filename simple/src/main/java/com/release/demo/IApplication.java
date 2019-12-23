648c3df02be8bfaeb5f8f36fdae18f58d3ccc8b6
Mr.release <cyc.release@gmail.com>
2019-12-23 12:21:22 +0800

    init commit

---
+++ simple/src/main/java/com/release/demo/IApplication.java
@@ -0,0 +1,20 @@
+package com.release.demo;
+
+import android.app.Application;
+
+/**
+ * @author Mr.release
+ * @create 2019-12-21
+ * @Describe
+ */
+public class IApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+//        IToolBar.init(R.color.Green);
+//        IToolBar.init(R.color.Purple,R.color.White,R.mipmap.toolbar_back_white);
+//        IToolBar.init(R.color.Blue,R.color.White,R.color.Yellow,R.mipmap.toolbar_back_white,120);
+    }
+}
