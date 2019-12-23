1dfc670152fa45039b0646ae745e09e106fd94e6
Mr.release <cyc.release@gmail.com>
2019-12-23 13:25:45 +0800

    init commit

--- simple/src/main/java/com/release/demo/IApplication.java
+++ simple/src/main/java/com/release/demo/IApplication.java
@@ -15,8 +15,11 @@ public class IApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
+        //第一种方式：只统一IToolBar背景色
 //        IToolBar.init(R.color.Green);
+        //第二种方式：统一IToolBar背景色、标题色、返回键图标
 //        IToolBar.init(R.color.Purple,R.color.White,R.mipmap.toolbar_back_white);
+        //第三种方式：统一IToolBar背景色、标题色、右边文本颜色、返回键图标、整体高度
 //        IToolBar.init(R.color.Blue,R.color.White,R.color.Yellow,R.mipmap.toolbar_back_white,120);
     }
 }
