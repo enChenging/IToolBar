593fa48ca9889dae6aafd6caca83d820218719a0
Mr.release <cyc.release@gmail.com>
2019-12-25 14:13:42 +0800

    init commit

--- itoolbar/src/main/java/com/release/itoolbar/IToolBar.java
+++ itoolbar/src/main/java/com/release/itoolbar/IToolBar.java
@@ -19,12 +19,12 @@ import androidx.annotation.Nullable;
  * @create 2019/4/25
  * @Describe
  */
-public class IToolBar extends FrameLayout {
+public class IToolBar extends FrameLayout implements View.OnClickListener {
 
     private ViewGroup toolBar;
     private ImageView iv_back;
     private TextView tv_title, tv_right;
-    private static int mThemeColor,mTitleColor,mRightTitleColor,mHight,mBackDrawable;
+    private static int mThemeColor, mTitleColor, mRightTitleColor, mHight, mBackDrawable;
 
     public IToolBar(Context context) {
         this(context, null);
@@ -70,21 +70,50 @@ public class IToolBar extends FrameLayout {
         if (mRightTitleColor != 0) tv_right.setTextColor(getResources().getColor(mRightTitleColor));
         if (mBackDrawable != 0) iv_back.setImageDrawable(getResources().getDrawable(mBackDrawable));
 
-        if (mHight != 0){
+        if (mHight != 0) {
             ViewGroup.LayoutParams layoutParams = toolBar.getLayoutParams();
             layoutParams.height = mHight;
             toolBar.setLayoutParams(layoutParams);
         }
 
         setBackFinish();
+        tv_right.setOnClickListener(this);
     }
 
+    private OnRightClickListener mOnRightClickListener;
+
+    @Override
+    public void onClick(View v) {
+        if (mOnRightClickListener != null) mOnRightClickListener.onClick(tv_right);
+    }
+
+    public interface OnRightClickListener {
+        public void onClick(TextView rightText);
+    }
+
+    public void setOnRightClickListener(OnRightClickListener clickListener) {
+        this.mOnRightClickListener = clickListener;
+    }
+
+
+    /**
+     * 背景色
+     *
+     * @param color
+     * @return
+     */
     public IToolBar setBackGroundColor(int color) {
         toolBar.setBackground(getResources().getDrawable(color));
         return this;
     }
 
 
+    /**
+     * 布局高度
+     *
+     * @param height
+     * @return
+     */
     public IToolBar setHeight(int height) {
         ViewGroup.LayoutParams layoutParams = toolBar.getLayoutParams();
         layoutParams.height = height;
@@ -92,49 +121,95 @@ public class IToolBar extends FrameLayout {
         return this;
     }
 
+    /**
+     * 使返回键消失
+     *
+     * @return
+     */
     public IToolBar setBackGone() {
         iv_back.setVisibility(View.INVISIBLE);
         return this;
     }
 
+    /**
+     * 设置返回键图标
+     *
+     * @param drawable
+     * @return
+     */
     public IToolBar setBackDrawable(Drawable drawable) {
         iv_back.setImageDrawable(drawable);
         return this;
     }
 
+    /**
+     * 设置返回键图标
+     *
+     * @param color
+     * @return
+     */
     public IToolBar setBackDrawable(int color) {
         iv_back.setImageDrawable(getResources().getDrawable(color));
         return this;
     }
 
-    public IToolBar setRight(String right, OnClickListener clickListener) {
+
+    /**
+     * 设置右边文本内容，及文本监听
+     *
+     * @param right
+     * @return
+     */
+    public IToolBar setRightText(String right) {
         tv_right.setVisibility(View.VISIBLE);
         tv_right.setText(right);
-        tv_right.setOnClickListener(clickListener);
         return this;
     }
 
-    public IToolBar setRight(int right, OnClickListener clickListener) {
+    /**
+     * 设置右边文本内容
+     *
+     * @param right
+     * @return
+     */
+    public IToolBar setRightText(int right) {
         tv_right.setVisibility(View.VISIBLE);
-        tv_right.setText(right);
-        tv_right.setOnClickListener(clickListener);
+        tv_right.setText(getResources().getText(right));
         return this;
     }
 
 
+    /**
+     * 设置右边文本显隐
+     *
+     * @param Visible
+     * @return
+     */
     public IToolBar setRightGone(int Visible) {
         tv_right.setVisibility(Visible);
         return this;
     }
 
 
+    /**
+     * 设置右边文本颜色
+     *
+     * @param color
+     * @return
+     */
     public IToolBar setRightTextColor(int color) {
         tv_right.setTextColor(getResources().getColor(color));
         return this;
     }
 
-    public IToolBar setRightSize(int textSize) {
-        tv_right.setTextSize((float) textSize);
+    /**
+     * 设置右边文本大小
+     *
+     * @param textSize
+     * @return
+     */
+    public IToolBar setRightSize(float textSize) {
+        tv_right.setTextSize(textSize);
         return this;
     }
 
@@ -149,26 +224,56 @@ public class IToolBar extends FrameLayout {
     }
 
 
+    /**
+     * 设置标题内容
+     *
+     * @param title
+     * @return
+     */
     public IToolBar setTitle(String title) {
         tv_title.setText(title);
         return this;
     }
 
+    /**
+     * 设置标题内容
+     *
+     * @param textId
+     * @return
+     */
     public IToolBar setTitle(int textId) {
-        tv_title.setText(textId);
+        tv_title.setText(getResources().getText(textId));
         return this;
     }
 
+    /**
+     * 设置标题颜色
+     *
+     * @param color
+     * @return
+     */
     public IToolBar setTitleColor(int color) {
         tv_title.setTextColor(getResources().getColor(color));
         return this;
     }
 
-    public IToolBar setTitleSize(int textSize) {
-        tv_title.setTextSize((float) textSize);
+    /**
+     * 设置标题大小
+     *
+     * @param textSize
+     * @return
+     */
+    public IToolBar setTitleSize(float textSize) {
+        tv_title.setTextSize(textSize);
         return this;
     }
 
+    /**
+     * 设置标题是否被选中
+     *
+     * @param b
+     * @return
+     */
     public IToolBar setTitleSelected(boolean b) {
         tv_title.setSelected(b);
         return this;
--- simple/src/main/java/com/release/demo/MainActivity.java
+++ simple/src/main/java/com/release/demo/MainActivity.java
@@ -1,7 +1,7 @@
 package com.release.demo;
 
 import android.os.Bundle;
-import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,7 +11,7 @@ import com.release.itoolbar.IToolBar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private IToolBar mTb1,mTb2,mTb3,mTb4;
+    private IToolBar mTb1, mTb2, mTb3, mTb4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +35,10 @@ public class MainActivity extends AppCompatActivity {
         mTb2.setTitle("标题2");
 
         mTb3.setTitle("标题3");
-        mTb3.setRight("分享", new View.OnClickListener() {
+        mTb3.setRightText("分享");
+        mTb3.setOnRightClickListener(new IToolBar.OnRightClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(TextView rightText) {
                 Toast.makeText(MainActivity.this, "分享", Toast.LENGTH_SHORT).show();
             }
         });
@@ -47,12 +48,12 @@ public class MainActivity extends AppCompatActivity {
         mTb4.setRightTextColor(R.color.White);
         mTb4.setBackGroundColor(R.color.Pink);
         mTb4.setBackDrawable(R.mipmap.toolbar_back_white);
-        mTb4.setRight("更多", new View.OnClickListener() {
+        mTb4.setRightText("更多");
+        mTb4.setOnRightClickListener(new IToolBar.OnRightClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(TextView rightText) {
                 Toast.makeText(MainActivity.this, "更多", Toast.LENGTH_SHORT).show();
             }
         });
-
     }
 }
